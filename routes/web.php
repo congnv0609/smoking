3a69574063e67c9d68767e2a248d03d50ee7ce5b
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-10-12 20:02:36 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::view('/{any}', 'admin.index')->where('any', '.*');
-});
+// Route::middleware(['auth:sanctum'])->group(function () {
+//     Route::view('/{any}', 'admin.index')->where('any', '.*');
+// });
 
-// Route::view('/{any}', 'admin.index')->where('any', '.*');
+Route::view('/{any}', 'admin.index')->where('any', '.*');
 // Route::view('/{any}', 'index')->where('any', '.*');
 
 // Route::any('/{any}',function(){
