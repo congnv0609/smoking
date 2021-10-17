654e9494a3127a344c8382fc189820128914fed3
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-10-17 18:49:42 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -65,7 +65,7 @@ echo "Copy files from git local to host site"
 source_directory="/var/www/html/ema-git/"
 des_directory="/var/www/html/ema-smoking"
 
-rsync -av --exclude={'/var/www/html/ema-git/deploy.sh','.git','/var/www/html/ema-git/storage','/var/www/html/ema-git/public/js','/var/www/html/ema-git/public/css','/var/www/html/ema-git/public/mix-manifest.json'} $source_directory $des_directory
+rsync -av --exclude={'var/www/html/ema-git/deploy.sh','.git','var/www/html/ema-git/storage','var/www/html/ema-git/public/js','var/www/html/ema-git/public/css','var/www/html/ema-git/public/mix-manifest.json'} $source_directory $des_directory
 
 cd $des_directory
 
