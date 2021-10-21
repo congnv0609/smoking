b42408ce875b3acc6956f4daaa54bbe5b80df234
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-10-21 17:27:35 +0700

    fix pagination bug

--- resources/admin/helpers/smoker.js
+++ resources/admin/helpers/smoker.js
@@ -1,6 +1,6 @@
 export function smokers(query) {
     return new Promise((resolve, reject) => {
-        axios.get('/backend/smokers/list', query)
+        axios.get('/backend/smokers/list', {params: query})
             .then(result => {
                 resolve(result.data);
             })
