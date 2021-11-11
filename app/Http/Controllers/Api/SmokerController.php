ff6ef5bbf53e4a178c78b044a146ea040622568b
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-11-11 18:30:17 +0700

    test

--- app/Http/Controllers/Api/SmokerController.php
+++ app/Http/Controllers/Api/SmokerController.php
@@ -360,8 +360,8 @@ class SmokerController extends Controller
         $url = 'https://fcm.googleapis.com/fcm/send';
         // $DeviceToken = Smoker::whereNotNull('device_token')->pluck('device_token')->all();
         $smoker = Smoker::whereNotNull('device_token')->where('id', $this->accountId)->first();
-        // $FcmKey = 'AAAAGOcfFW8:APA91bFltHXEGi6__AWHagTK2cv6T3tEbxydQsKKFrQriX14fhx0e5Elerf9CFIu_MerWA6J7e4fQEBtmAi9LMOGijROedN8UWelgeTaf1Mg8U4_kCRnKkYM9eczWYFNKuIEfMA2N8Ya';
         $FcmKey = 'AAAAGOcfFW8:APA91bFltHXEGi6__AWHagTK2cv6T3tEbxydQsKKFrQriX14fhx0e5Elerf9CFIu_MerWA6J7e4fQEBtmAi9LMOGijROedN8UWelgeTaf1Mg8U4_kCRnKkYM9eczWYFNKuIEfMA2N8Ya';
+        // $FcmKey = 'AAAAGOcfFW8:APA91bFltHXEGi6__AWHagTK2cv6T3tEbxydQsKKFrQriX14fhx0e5Elerf9CFIu_MerWA6J7e4fQEBtmAi9LMOGijROedN8UWelgeTaf1Mg8U4_kCRnKkYM9eczWYFNKuIEfMA2N8Ya';
         // $FcmKey = env('FCM');
         $data = [
             "registration_ids" => [$smoker->device_token],
