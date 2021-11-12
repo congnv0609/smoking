9baa8d7a0191ca59b1b4177a9f9d886d6da6d01e
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-11-12 17:57:39 +0700

    update push

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,25 +28,25 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('ema:get-schedule')->daily();
+        $schedule->command('ema:get-schedule')->daily();
         // $schedule->command('ema:get-schedule');
-        // $data = Cache::get('ema:schedule');
-        $value = [
-            "id" => 134,
-            "account_id" => 34,
-            "date" => "2021-11-12",
-            "popup_time" => "2021-11-12 21:30:00",
-            "postponded_1" => 0,
-            "postponded_2" => 0,
-        ];
-        $schedule->job(new SendNotification($value))->everyMinute();
-        // if (!empty($data)) {
-        //     foreach ($data as $key => $value) {
-        //         $time = date_format(new DateTime($value["popup_time"]), 'H:i');
-        //         $schedule->job(new SendNotification($value))->daily()->at($time);
-        //         // $schedule->job(new SendNotification($value))->everyMinute();
-        //     }
-        // }
+        $data = Cache::get('ema:schedule');
+        // $value = [
+        //     "id" => 134,
+        //     "account_id" => 34,
+        //     "date" => "2021-11-12",
+        //     "popup_time" => "2021-11-12 21:30:00",
+        //     "postponded_1" => 0,
+        //     "postponded_2" => 0,
+        // ];
+        // $schedule->job(new SendNotification($value))->everyMinute();
+        if (!empty($data)) {
+            foreach ($data as $key => $value) {
+                $time = date_format(new DateTime($value["popup_time"]), 'H:i');
+                $schedule->job(new SendNotification($value))->daily()->at($time);
+                // $schedule->job(new SendNotification($value))->everyMinute();
+            }
+        }
     }
 
     /**
