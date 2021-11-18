8e04a4ab6889a5a01d8435e20dc270482c323bb8
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-11-18 16:05:53 +0700

    add postponded to push

--- app/Jobs/SendNotification.php
+++ app/Jobs/SendNotification.php
@@ -49,7 +49,7 @@ class SendNotification implements ShouldQueue
         $FcmKey = 'AAAAGOcfFW8:APA91bFltHXEGi6__AWHagTK2cv6T3tEbxydQsKKFrQriX14fhx0e5Elerf9CFIu_MerWA6J7e4fQEBtmAi9LMOGijROedN8UWelgeTaf1Mg8U4_kCRnKkYM9eczWYFNKuIEfMA2N8Ya';
         // $FcmKey = env('FCM');
         $ema = $this->getPopupTime($this->_ema['account_id']);
-        
+
         $info = $this->getPromptMessage($ema);
         $data = [
             "registration_ids" => [$smoker->device_token],
@@ -58,7 +58,7 @@ class SendNotification implements ShouldQueue
                 "body" => $info["body"],
                 'sound' => $smoker->notification == 1 ? "default" : "",
             ],
-            "data" => ["current_ema" => $ema->current_ema, "ema"=>$ema->ema, "nth_popup"=>$ema->nth_popup],
+            "data" => ["current_ema" => $ema->current_ema, "ema" => $ema->ema, "nth_popup" => $ema->nth_popup, "postponded_1" => $ema->postponded_1, "postponded_2" => $ema->postponded_2, "postponded_3" => $ema->postponded_3],
         ];
         $this->updateCountPush($ema);
         Artisan::call('ema:get-schedule');
@@ -87,5 +87,4 @@ class SendNotification implements ShouldQueue
         curl_close($ch);
         // dd($output);
     }
-
 }
