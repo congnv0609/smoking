64405f17d29dd325a97ec1ad11a75fadf83ddc6a
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-11-22 09:38:56 +0700

    update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -45,15 +45,15 @@ class Kernel extends ConsoleKernel
             foreach ($data as $key => $value) {
                 if ($value["nth_popup"] == 1) {
                     $time = date_format(new DateTime($value["popup_time"]), 'H:i');
-                    $schedule->job(new SendNotification($value))->at($time);
+                    $schedule->job(new SendNotification($value))->daily()->at($time);
                 }
                 if ($value["postponded_1"] == null && $value["nth_popup"] == 2) {
                     $time1 = date_format(new DateTime($value["popup_time1"]), 'H:i');
-                    $schedule->job(new SendNotification($value))->at($time1);
+                    $schedule->job(new SendNotification($value))->daily()->at($time1);
                 }
                 if ($value["postponded_1"] == null && $value["nth_popup"] == 3) {
                     $time2 = date_format(new DateTime($value["popup_time2"]), 'H:i');
-                    $schedule->job(new SendNotification($value))->at($time2);
+                    $schedule->job(new SendNotification($value))->daily()->at($time2);
                 }
             }
         }
