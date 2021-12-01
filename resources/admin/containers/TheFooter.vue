dc4b575bf81a67e9728002b50966308b6f25abca
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-12-01 10:50:14 +0700

    update footer

--- resources/admin/containers/TheFooter.vue
+++ resources/admin/containers/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
+      <a href="https://smoking.nursing.hku.hk" target="_blank">EMA Project</a>
+      <span class="ml-1">&copy; {{new Date().getFullYear()}}</span>
     </div>
     <div class="mfs-auto">
       <span class="mr-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <a href="https://smoking.nursing.hku.hk">HKU</a>
     </div>
   </CFooter>
 </template>
