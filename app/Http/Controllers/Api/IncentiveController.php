294a277be62a0cc2f237a020edd262f6744eeee9
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2021-12-12 12:00:38 +0700

    update finished survey

--- app/Http/Controllers/Api/IncentiveController.php
+++ app/Http/Controllers/Api/IncentiveController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Models\Incentive;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Ema1;
+use App\Models\Ema2;
+use App\Models\Ema3;
+use App\Models\Ema4;
+use App\Models\Ema5;
+use Illuminate\Support\Arr;
 
 class IncentiveController extends Controller
 {
@@ -22,7 +28,31 @@ class IncentiveController extends Controller
      * 
      */
     public function finished(){
-        $data = Incentive::select('date','valid_ema', 'updated_at')->where('account_id', $this->accountId)->get();
+        $data = [];
+        // $data = Incentive::select('date','valid_ema', 'updated_at')->where('account_id', $this->accountId)->get();
+        $ema1 = Ema1::select('id', 'account_id', 'date', 'nth_day', 'nth_ema', 'submit_time', 'completed')->where([['completed', true],['account_id', $this->accountId]])->get();
+        $ema2 = Ema2::select('id', 'account_id', 'date', 'nth_day', 'nth_ema', 'submit_time', 'completed')->where([['completed', true],['account_id', $this->accountId]])->get();
+        $ema3 = Ema3::select('id', 'account_id', 'date', 'nth_day', 'nth_ema', 'submit_time', 'completed')->where([['completed', true],['account_id', $this->accountId]])->get();
+        $ema4 = Ema4::select('id', 'account_id', 'date', 'nth_day', 'nth_ema', 'submit_time', 'completed')->where([['completed', true],['account_id', $this->accountId]])->get();
+        $ema5 = Ema5::select('id', 'account_id', 'date', 'nth_day', 'nth_ema', 'submit_time', 'completed')->where([['completed', true],['account_id', $this->accountId]])->get();
+        if(!empty($ema1)) {
+            $data[]=$ema1->toArray();
+        }
+        if (!empty($ema2)) {
+            $data[] = $ema2->toArray();
+        }
+        if (!empty($ema3)) {
+            $data[] = $ema3->toArray();
+        }
+        if (!empty($ema4)) {
+            $data[] = $ema4->toArray();
+        }
+        if (!empty($ema5)) {
+            $data[] = $ema5->toArray();
+        }
+        $data = Arr::collapse($data);
+        $data = collect($data)->sortBy('nth_day')->toArray();
+        $data = array_values($data);
         return response()->json($data, 200);
     }
 
