b486d7bc95b4b38d1042d4f8196c266665528953
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2022-02-11 11:08:27 +0700

    change priority push

--- app/Jobs/SendNotification.php
+++ app/Jobs/SendNotification.php
@@ -54,7 +54,8 @@ class SendNotification implements ShouldQueue
             "notification" => [
                 "title" => $info["title"],
                 "body" => $info["body"],
-                'sound' => $smoker->notification == 1 ? "default" : null,
+                "sound" => $smoker->notification == 1 ? "default" : null,
+                "notification_priority" => "PRIORITY_MAX",
             ],
             "data" => ["current_ema" => $ema['current_ema'], "ema" => $ema['nth_ema'], "nth_popup" => $ema['nth_popup'], 'nth_day'=>$ema['nth_day'], "postponded_1" => $ema['postponded_1'], "postponded_2" => $ema['postponded_2'], "postponded_3" => $ema['postponded_3']],
         ];
