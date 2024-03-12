bf7fc85c0fd7955caf22c6fef8faffab08cdab60
congnv <50324810+leonardz1z@users.noreply.github.com>
2024-03-12 09:25:01 +0700

    last commit

--- app/Console/Commands/ReportUser.php
+++ app/Console/Commands/ReportUser.php
@@ -8,6 +8,7 @@ use App\Models\Ema3;
 use App\Models\Ema4;
 use App\Models\Ema5;
 use App\Models\Smoker;
+use DateTime;
 use Illuminate\Console\Command;
 
 class ReportUser extends Command
@@ -65,7 +66,7 @@ class ReportUser extends Command
         $data['prompt_ema'] = $this->countEmaByPrompt($accountId);
         $data['response_ema'] = $this->countEmaByCompleted($accountId);
         $data['non_response_ema'] = ($data['prompt_ema'] - $data['response_ema']) > 0 ? $data['prompt_ema'] - $data['response_ema']: 0;
-        $data['future_ema'] = 35 - $data['prompt_ema'];
+        $data['future_ema'] = $this->countFutureEma($accountId);
         $data['response_rate'] = $data['prompt_ema'] > 0 ? $data['response_ema'] / $data['prompt_ema'] : 0;
         return $data;
     }
@@ -89,6 +90,17 @@ class ReportUser extends Command
         $count += Ema3::where([['completed', true], ['account_id', $accountId]])->count();
         $count += Ema4::where([['completed', true], ['account_id', $accountId]])->count();
         $count += Ema5::where([['completed', true], ['account_id', $accountId]])->count();
+        return $count;
+    }
+
+    public function countFutureEma($accountId)
+    {
+        $count = 0;
+        $count += Ema1::where([['popup_time', '>', new DateTime()], ['account_id', $accountId]])->count();
+        $count += Ema2::where([['popup_time', '>', new DateTime()], ['account_id', $accountId]])->count();
+        $count += Ema3::where([['popup_time', '>', new DateTime()], ['account_id', $accountId]])->count();
+        $count += Ema4::where([['popup_time', '>', new DateTime()], ['account_id', $accountId]])->count();
+        $count += Ema5::where([['popup_time', '>', new DateTime()], ['account_id', $accountId]])->count();
         return $count;
     }
 }
